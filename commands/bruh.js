0fcee4711eb43ed839974798fc9520fdfdb5ddc1
BuildTools <unconfigured@null.spigotmc.org>
2020-07-28 14:36:34 -0700

    added bruh

--- commands/bruh.js
+++ commands/bruh.js
@@ -2,6 +2,8 @@ module.exports = {
 	name: 'ping',
 	description: 'reacts with bruh when someone says bruh',
 	execute(message) {
-		
+		if (message.content === 'bruh') {
+            message.react('🗿');
+        }
 	},
 };
