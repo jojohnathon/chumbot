62697567a8d20427b80332c1c36ae242dc46ad32
BuildTools <unconfigured@null.spigotmc.org>
2020-07-28 11:12:37 -0700

    testing command handler

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ const { MessageEmbed } = require('discord.js');
 
 const ping = require('minecraft-server-util');
 
+const fs = require('fs');
+
+bot.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles) {
+	const command = require('`./commands/${file}');
+
+	client.commands.set(command.name, command);
+}
+	
 
 bot.on('ready', () =>{
 	console.log('Bot has come online.');
@@ -40,6 +51,17 @@ bot.on('message', message =>{
 
 	}
 
+});
+
+bot.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+ 
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+ 
+    if(command === 'ping') {
+        bot.commands.get('ping').execute(message, args);
+    } 
 });
 
 bot.login(process.env.token);
