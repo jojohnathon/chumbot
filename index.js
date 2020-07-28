459e75d28ba3d46871a2739115901a118c341d7f
BuildTools <unconfigured@null.spigotmc.org>
2020-07-28 11:16:03 -0700

    tweaked command handler

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith(
 for(const file of commandFiles) {
 	const command = require('`./commands/${file}');
 
-	client.commands.set(command.name, command);
+	bot.commands.set(command.name, command);
 }
 	
 
