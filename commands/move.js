524beb3d14524b27fea6b41cc702a89962ad3100
BuildTools <unconfigured@null.spigotmc.org>
2020-07-30 09:05:47 -0700

    move

--- commands/move.js
+++ commands/move.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'move',
 	description: 'moves people!',
 	execute(message) {
-		client.GuildMember.setVoiceChannel(719312319808798810);
+		Discord.GuildMember.setVoiceChannel(719312319808798810);
 	},
 };
 
