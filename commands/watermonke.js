b981b71be431cc805b9fc3f358d377b31d9bf3bf
BuildTools <unconfigured@null.spigotmc.org>
2020-08-25 18:01:35 -0700

    water momek

--- commands/watermonke.js
+++ commands/watermonke.js
@@ -4,12 +4,7 @@ module.exports = {
 	name: 'watermonkey',
 	description: 'water monkey',
 	execute(message) {
-       let args = message.content.substring(prefix.length).split(" ");
-
-       switch(args[0]){
-           case 'sendlocal':
-               const attachment = new Attachment('./images/watermonke.png');
-               message.Channel.send(message.author, attachment2)
+        const attachment = new Attachment('./images/watermonke.png');
+        message.Channel.send(message.author, attachment2)
        }
-	},
-};
+	};
