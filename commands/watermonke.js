8428743c807c83aa20d20e56d881ee52714d3775
BuildTools <unconfigured@null.spigotmc.org>
2020-08-25 17:47:29 -0700

    fixed water monke

--- commands/watermonke.js
+++ commands/watermonke.js
@@ -4,15 +4,6 @@ module.exports = {
 	name: 'watermonkey',
 	description: 'water monkey',
 	execute(message) {
-        const file = new Discord.MessageAttachment('../images/watermonke.png');
-        
-        const exampleEmbed = {
-            title: '',
-            image: {
-                url: 'attachment://watermonke.png',
-            },
-        };
-        
-        Channel.send({ files: [file], embed: exampleEmbed });
+       message.Channel.send ('water monke', {files: ['./images/watermonke.js']})
 	},
 };
