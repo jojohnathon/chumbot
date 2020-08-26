a5668c499f685eff51a8bba2c81503f1f130b80a
BuildTools <unconfigured@null.spigotmc.org>
2020-08-25 18:06:32 -0700

    added err reporting for water monkey

--- commands/watermonke.js
+++ commands/watermonke.js
@@ -6,5 +6,8 @@ module.exports = {
 	execute(message) {
         const attachment = new Attachment('./images/watermonke.png');
         message.Channel.send(message.author, attachment2)
+        if (error) {
+            console.log(error);
+        }
        }
 	};
